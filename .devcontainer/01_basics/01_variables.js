a1f4f640ddf3dac44185976f3b10e5f71b5a231b
zaynsheat <72312723+zaynsheat@users.noreply.github.com>
2023-09-14 15:30:49 +0000

    before download

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+/* Declaring Variables by different methods like: */
+
+const userRollnumber = 10 //declaring constant
+let userName
+userName = "Nyaz"
+console.log(userName);
+console.log(
---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,17 @@
+/*There's no alert in Javascript if you use Node JS, 
+alert method only works in Browser Engine*/
+
+// semicolons not necessary 
+
+/* ECMA Script (tc39.es) is the official documentation of JS,
+ MDN can be used which is created by Mozilla */
+
+ /*
+ Datatypes in JS:
+ -Number
+ -bigint
+ -string = ""
+ -boolean = true/ false
+ -null = Standalone value
+ -undefined =   
+ */ 
